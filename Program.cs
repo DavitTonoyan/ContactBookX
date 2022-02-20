9531ab8a02fbecef08e2aee6a29672838d302209
DavitTonoyan <davit.tonoian@gmail.com>
2022-02-20 00:26:42 +0000

    a

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ContactBookX
     {
         static async Task Main(string[] args)
         {
-            // Please before start the project  GOTO -> Tables -> CBook.mdf
+            // Please before start the project  go to Tables -> CBook.mdf
             // and update tables to your Database
 
             ILogger logger = Logger.CreateInstance();
